297072971eadc73d0005fc99ccfb5520ed79030d
Aleksandra Zografska <aleksndra2@gmail.com>
2020-03-28 00:38:33 +0100

    Adding student file

---
+++ Student.java
@@ -0,0 +1,19 @@
+
+
+class Student {
+    String index;
+    String firstName;
+    String lastName;
+
+    //TODO constructor
+
+    //TODO seters & getters
+
+    public double getAverage() {
+        //TODO
+    }
+
+    public int ECTSCredits() {
+        //TODO
+    }
+}
