c1be9851b97369f1cbcbbbed80281a908de351d9
Aleksandra Zografska <aleksndra2@gmail.com>
2020-03-28 01:24:44 +0100

    student class finished

--- Student.java
+++ Student.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.OptionalDouble;
 
 public class Student {
     String index;
@@ -7,14 +8,50 @@ public class Student {
     ArrayList<Integer> grades;
     //TODO constructor
 
-    //TODO seters & getters
+    public Student(String index, String firstName, String lastName,ArrayList<Integer> grades) {
+        this.index = index;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.grades = grades;
+    }
+
+    public String getIndex() {
+        return index;
+    }
+
+    public void setIndex(String index) {
+        this.index = index;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setGrades(ArrayList<Integer> grades) {
+        this.grades = grades;
+    }
+
+    public ArrayList<Integer> getGrades() {
+        return grades;
+    }
 
     public double getAverage() {
-        //TODO
+        return grades.stream().mapToInt(Integer::intValue).average().getAsDouble();
     }
 
     public int ECTSCredits() {
-        //TODO
+        return (int) (grades.stream().filter(x -> x>5).count() * 6);
     }
 }
-
